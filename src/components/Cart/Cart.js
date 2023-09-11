ce9bff174e523093fa113325c963b38c3abfff47
Madalina <madalina@just-plus.com>
2023-09-11 12:04:10 +0300

    first commit

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,51 @@
+import React, { useContext } from "react";
+import classes from "./Cart.module.css";
+import Modal from "../UI/Modal";
+import CartContext from "../../store/cart-context";
+import CartItem from "./CartItem";
+
+export const Cart = (props) => {
+  const cartCtx = useContext(CartContext);
+  
+  const cartItemRemoveHandler = (id) => {
+    cartCtx.removeItem(id);
+  }
+
+  const cartItemAddHandler = (item) => {
+    cartCtx.addItem(item)
+  }
+  const cartItems = (
+    <ul className={classes["cart-items"]}>
+      {[
+        cartCtx.items.map((item) => (
+          <CartItem
+            key={item.id}
+            name={item.name}
+            amount={item.amount}
+            price={item.price}
+            onAdd={cartItemAddHandler.bind(null, item)}
+            onRemove={cartItemRemoveHandler.bind(null,item.id)}
+          />
+        )),
+      ]}
+    </ul>
+  );
+  
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+  const hasItems = cartCtx.items.length > 0;
+  return (
+    <Modal onHideCart={props.onHideCart}>
+      {cartItems}
+      <div className={classes.total}>
+        <span>Total Amount</span>
+        <span>{totalAmount} </span>
+      </div>
+      <div className={classes.actions}>
+        <button className={classes["button--alt"]} onClick={props.onHideCart}>
+          Close
+        </button>
+        {hasItems && <button className={classes.button}>Order</button>}
+      </div>
+    </Modal>
+  );
+};
---
+++ src/components/UI/Input.js
@@ -0,0 +1,11 @@
+import React , {forwardRef} from 'react'
+import classes from './Input.module.css'
+
+export const Input = forwardRef((props, ref) => {
+  return (
+    <div className={classes.input}>
+        <label htmlFor={props.input.id}>{props.label}</label>
+        <input ref={ref} type="text" {...props.input}/>
+    </div>
+  )
+})
