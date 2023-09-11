a8e854d35e2edba56cd2fde28c20c2cbd3742d24
Madalina <madalina@just-plus.com>
2023-09-11 14:45:26 +0300

    http requests with firebase and confirm order form

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,19 +1,49 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import classes from "./Cart.module.css";
 import Modal from "../UI/Modal";
 import CartContext from "../../store/cart-context";
 import CartItem from "./CartItem";
+import Checkout from "./Checkout";
 
 export const Cart = (props) => {
   const cartCtx = useContext(CartContext);
-  
+  const [isCheckout, setIsCheckout] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [didSubmit, setDidSubmit] = useState(false);
+
   const cartItemRemoveHandler = (id) => {
     cartCtx.removeItem(id);
-  }
+  };
 
   const cartItemAddHandler = (item) => {
-    cartCtx.addItem(item)
-  }
+    cartCtx.addItem(item);
+  };
+
+  const orderHandler = () => {
+    setIsCheckout(true);
+  };
+
+  const submitOrderHandler = async (userData) => {
+    setIsSubmitting(true);
+    try {
+      const response = await fetch(
+        "https://react-max-project-default-rtdb.europe-west1.firebasedatabase.app/orders.json",
+        {
+          method: "POST",
+          body: JSON.stringify({ user: userData, orderedItems: cartCtx.items }),
+        }
+      );
+      if (!response.ok) {
+        throw new Error("Something went wrong");
+      }
+      setIsSubmitting(false);
+      setDidSubmit(true);
+      cartCtx.clearCart();
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   const cartItems = (
     <ul className={classes["cart-items"]}>
       {[
@@ -24,28 +54,60 @@ export const Cart = (props) => {
             amount={item.amount}
             price={item.price}
             onAdd={cartItemAddHandler.bind(null, item)}
-            onRemove={cartItemRemoveHandler.bind(null,item.id)}
+            onRemove={cartItemRemoveHandler.bind(null, item.id)}
           />
         )),
       ]}
     </ul>
   );
-  
+
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
   const hasItems = cartCtx.items.length > 0;
-  return (
-    <Modal onHideCart={props.onHideCart}>
+
+  const modalActions = (
+    <div className={classes.actions}>
+      <button className={classes["button--alt"]} onClick={props.onHideCart}>
+        Close
+      </button>
+      {hasItems && (
+        <button className={classes.button} onClick={orderHandler}>
+          Order
+        </button>
+      )}
+    </div>
+  );
+
+  const cartModalContent = (
+    <>
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
         <span>{totalAmount} </span>
       </div>
+      {isCheckout && (
+        <Checkout onConfirm={submitOrderHandler} onCancel={props.onHideCart} />
+      )}
+      {!isCheckout && modalActions}
+    </>
+  );
+
+  const didSubmitModalContent = (
+    <>
+      <p>Successfully sent the order!</p>
       <div className={classes.actions}>
-        <button className={classes["button--alt"]} onClick={props.onHideCart}>
-          Close
-        </button>
-        {hasItems && <button className={classes.button}>Order</button>}
-      </div>
+      <button className={classes["button--alt"]} onClick={props.onHideCart}>
+        Close
+      </button>
+    </div>
+    </>
+  );
+
+  const isSubmittingModalContent = <p>Sending order data...</p>;
+  return (
+    <Modal onHideCart={props.onHideCart}>
+      {!isSubmitting && !didSubmit && cartModalContent}
+      {isSubmitting && !didSubmit && isSubmittingModalContent}
+      {!isSubmitting && didSubmit && didSubmitModalContent}
     </Modal>
   );
 };
